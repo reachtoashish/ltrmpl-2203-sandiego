fd034a9e916f16df8ad444570a38b6574130ee2e
Ashish Panda <reachtoashish@gmail.com>
2019-05-24 04:51:39 -0400

    RR BGP preconfig added in ssh onit script

--- misc/ssh_key_init.py
+++ misc/ssh_key_init.py
@@ -27,6 +27,17 @@ yes
 exit
 """
 
+rr_config = """
+conf t
+router bgp 1
+ address-family vpnv4 unicast
+ !
+root
+commit
+exit
+exit
+"""
+
 def log_info(message_string):
     # print message_string
     ts = time.gmtime()
@@ -59,6 +70,7 @@ def read_output(tn, command):
 IOS_HOST = ["198.18.1.111", "198.18.1.112"]
 XE_HOST = ["198.18.1.12"]
 XR_HOST = ["198.18.1.11", "198.18.1.13", "198.18.1.14", "198.18.1.100", "198.18.1.200"]
+RR_HOST = ["198.18.1.100", "198.18.1.200"]
 
 # user = raw_input("Enter your remote account: ")
 # password = getpass.getpass()
@@ -93,3 +105,11 @@ for HOST in XR_HOST:
     post_login.write(xr_config + "\n")
     log_info('Key generated for Host: %s' % HOST)
     time.sleep(5)
+
+for HOST in RR_HOST:
+    telnet_obj = telnetlib.Telnet(HOST)
+    post_login = login_router(telnet_obj, user, password)
+    post_login.write(rr_config + "\n")
+    log_info('RR BGP pre config pushed: %s' % HOST)
+    time.sleep(5)
+
